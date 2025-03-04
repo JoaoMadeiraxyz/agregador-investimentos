1d1a4034c34ee653362e92984f4ec5d4124c7356
JoaoMadeiraxyz <jaodisc@gmail.com>
2025-03-04 20:11:28 -0300

    feat: implement create user endpoint TODO: hash the password before saving

---
+++ src/main/java/com/joaomadeiraxyz/agregador_investimentos/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.joaomadeiraxyz.agregador_investimentos.repository;
+
+import com.joaomadeiraxyz.agregador_investimentos.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
---
+++ src/main/java/com/joaomadeiraxyz/agregador_investimentos/service/UserService.java
@@ -0,0 +1,33 @@
+package com.joaomadeiraxyz.agregador_investimentos.service;
+
+import com.joaomadeiraxyz.agregador_investimentos.controller.CreateUserDto;
+import com.joaomadeiraxyz.agregador_investimentos.entity.User;
+import com.joaomadeiraxyz.agregador_investimentos.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UUID createUser(CreateUserDto createUserDto) {
+
+        var entity = new User(null,
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null);
+
+        var userSaved =  userRepository.save(entity);
+
+        return userSaved.getUserId();
+    }
+}
